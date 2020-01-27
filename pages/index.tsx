54ad9093282b643aad059541a1509bc715f7739e
EnderDev <xenderpugx@gmail.com>
2020-01-27 21:31:20 +0000

    ✨ Created NextJS app

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+const App = () => (
+    <html lang="en">
+        <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <title>Cards Against Mankind</title>
+        </head>
+        <body>
+            App
+        </body>
+    </html>
+)
+
+export default App;
